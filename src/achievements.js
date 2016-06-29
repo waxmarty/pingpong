14805dc9802e8deabe3fa1cc595b8304b6262c2b
waxmarty <waxmartin1337@gmail.com>
2016-06-29 17:31:49 +0200

    Add more Achievements
    Still need to think about them

--- src/achievements.js
+++ src/achievements.js
@@ -1,12 +1,24 @@
 callAchievement = () => {
   let score = localStorage.getItem('score')
+  let gamesPlayed = JSON.parse(localStorage.getItem('games')).length
 
   if(score >= 100 && !localStorage.getItem('firstAchievement')) {
     alertify.success('You got more than 100 points!')
     localStorage.setItem('firstAchievement', true)
   }
+
   if(score >= 200 && !localStorage.getItem('secondAchievement')) {
     alertify.success('You got more than 200 points!')
     localStorage.setItem('secondAchievement', true)
+  }
+
+  if(gamesPlayed === 50 && !localStorage.getItem('50gAchievement')) {
+    alertify.success('You played 50 games!')
+    localStorage.setItem('50gAchievement', true)
+  }
+
+  if(gamesPlayed === 100 && !localStorage.getItem('100gAchievement')) {
+    alertify.success('You played 100 games!')
+    localStorage.setItem('100gAchievement', true)
   }
 }
