1538fce123a300b347bddb9f2468632d9ff76d38
waxmarty <waxmartin1337@gmail.com>
2016-06-29 16:14:54 +0200

    Reindent

--- src/index.js
+++ src/index.js
@@ -2,66 +2,66 @@ if(require) {
 	alertify = require('alertify.js')
 }
 
-if(!localStorage.getItem('score')) {
-  localStorage.setItem('score', 10)
-	localStorage.setItem('games', JSON.stringify({}))
-
-  document.getElementById('gameScore')
-    .innerHTML = localStorage.getItem('score')
-} else {
-  document.getElementById('gameScore')
-    .innerHTML = localStorage.getItem('score')
-}
-
 alertify.logPosition('top right')
 
-play = (side) => {
-  const num = Math.floor((Math.random() * 10))
+if(!localStorage.getItem('score')) {
+	localStorage.setItem('score', 10)
+	localStorage.setItem('games', JSON.stringify({}))
 
-  switch(side) {
-    case 'right':
-      if(num > 5) {
-        won(side)
-      } else {
-        lose()
-      }
-      break;
-    case 'left':
-      if(num < 6) {
-        won()
-      } else {
-        lose()
-      }
-      break;
-  }
+	document.getElementById('gameScore')
+		.innerHTML = localStorage.getItem('score')
+} else {
+	document.getElementById('gameScore')
+		.innerHTML = localStorage.getItem('score')
+}
+
+play = (side) => {
+	const num = Math.floor((Math.random() * 10))
+
+	switch(side) {
+		case 'right':
+			if(num > 5) {
+				won(side)
+			} else {
+				lose()
+			}
+			break;
+		case 'left':
+			if(num < 6) {
+				won()
+			} else {
+				lose()
+			}
+			break;
+	}
 }
 
 won = (side) => {
-  console.log('won')
-  let afterScore = parseInt(localStorage.getItem('score')) + 7
+	console.log('won')
+	let afterScore = parseInt(localStorage.getItem('score')) + 7
 
-  localStorage.setItem('score', afterScore)
+	localStorage.setItem('score', afterScore)
 
 	pushGame(side, 'won')
 
-  alertify.success('You won +1')
+	alertify.success('You won +1')
 
-  document.getElementById('gameScore')
-    .innerHTML = localStorage.getItem('score')
+	document.getElementById('gameScore')
+		.innerHTML = localStorage.getItem('score')
 }
 
 lose = (side) => {
-  console.log('lose')
-  let afterScore = parseInt(localStorage.getItem('score')) - 5
+	console.log('lose')
+	let afterScore = parseInt(localStorage.getItem('score')) - 5
 
-  localStorage.setItem('score', afterScore)
+	localStorage.setItem('score', afterScore)
 
 	pushGame(side, 'lose')
 
-  alertify.error('You lose! -1')
+	alertify.error('You lose! -1')
 
-  document.getElementById('gameScore')
-    .innerHTML = localStorage.getItem('score')
+	document.getElementById('gameScore')
+		.innerHTML = localStorage.getItem('score')
 }
 
 pushGame = (side, result) => {
--- src/menu.js
+++ src/menu.js
@@ -3,36 +3,37 @@ const { Menu, MenuItem } = remote
 
 const template = [
   {
-		label: 'Game',
-		submenu: [
-			{
-				label: 'Restart game',
-				click() {
-					localStorage.setItem('score', 10)
-				}
-			},
-			{
+    label: 'Game',
+    submenu: [
+      {
+        label: 'Restart game',
+        click() {
+          localStorage.setItem('score', 10)
+        }
+      },
+      {
         label: 'Toggle Developer Tools',
         accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
         click(item, focusedWindow) {
-          if (focusedWindow)
+          if (focusedWindow) {
             focusedWindow.webContents.toggleDevTools();
+          }
         }
       },
-			{
-      	type: 'separator'
-	    },
-			{
-	      label: 'Quit',
-	      accelerator: 'CmdOrCtrl+Q',
-	      role: 'quit'
-	    },
-	    {
-	      label: 'Minimize',
-	      accelerator: 'CmdOrCtrl+M',
-	      role: 'minimize'
-	    }
-		]
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        role: 'quit'
+      },
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      }
+    ]
   }
 ]
 
